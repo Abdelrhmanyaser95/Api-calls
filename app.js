ecaa0ceed6e0b86c2c3e92451f2be343f4119843
Abdelrhman yasser <130260479+Abdelrhmanyaser95@users.noreply.github.com>
2024-06-26 13:11:00 +0300

    Add files via upload
    create api calls

---
+++ app.js
@@ -0,0 +1,26 @@
+const postsListContainer = document.querySelector(".posts-list-container");
+function fetchUsingFetchMethod() {
+    const fetchRequest = fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "GET",
+    });
+  
+    fetchRequest
+      .then((response) => response.json())
+      .then((result) => displayResults(result))
+      .catch((e) => console.log(e));
+  }
+
+
+  function displayResults(posts) {
+    postsListContainer.innerHTML = posts
+      .map(
+        (postItem) => `
+      <div class="post-item">
+      <h3>${postItem.title}</h3>
+      <p>${postItem.body}</p>
+      </div>
+      `
+      )
+      .join(" ");
+  }  
+  fetchUsingFetchMethod();
